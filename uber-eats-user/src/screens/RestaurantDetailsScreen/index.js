071b188f1f62b2b7829623d8c47b48c37b62de39
NRotondaro <niksonrotondaro1@gmail.com>
2022-06-14 19:58:43 -0300

    feat(menu-list): added the menu to the res screen

--- uber-eats-user/src/screens/RestaurantDetailsScreen/index.js
+++ uber-eats-user/src/screens/RestaurantDetailsScreen/index.js
@@ -1,57 +1,28 @@
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import DishListItem from '../../components/DishListItem';
 import restaurants from '../../../assets/data/restaurants.json';
+import Header from './Header';
+import styles from './styles';
 
 const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
   return (
     <View style={styles.page}>
-      <Image source={{ uri: restaurant.image }} style={styles.image} />
-
+      <FlatList
+        ListHeaderComponent={() => <Header restaurant={restaurant} />}
+        data={restaurant.dishes}
+        renderItem={({ item }) => <DishListItem dish={item} />}
+      />
       <Ionicons
         name='arrow-back-circle'
         size={45}
         color='white'
         style={styles.icon}
       />
-
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>
-          $ {restaurant.deliveryFee} · {restaurant.minDeliveryTime}-
-          {restaurant.maxDeliveryTime} minutes
-        </Text>
-      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-  },
-  container: {
-    margin: 10,
-  },
-  icon: {
-    position: 'absolute',
-    top: 40,
-    left: 10,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 5 / 3,
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  subtitle: {
-    fontSize: 15,
-    color: 'gray',
-  },
-});
 
 export default RestaurantDetailsPage;
