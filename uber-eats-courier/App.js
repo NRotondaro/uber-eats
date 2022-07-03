022acec0918a4e14e5e6e15f373e59a243a85d4b
NRotondaro <niksonrotondaro1@gmail.com>
2022-07-03 23:01:21 +0000

    feat(orders): created the screen and implemented bottom sheet

--- uber-eats-courier/App.js
+++ uber-eats-courier/App.js
@@ -1,26 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, View } from 'react-native';
-import orders from './assets/data/orders.json';
-import OrderItem from './src/components/OrderItem';
+import { View } from 'react-native';
+import OrdersScreen from './src/screens/OrdersScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={orders}
-        renderItem={({ item }) => <OrderItem order={item} />}
-      />
-
+    <View style={{ flex: 1, justifyContent: 'center', paddingTop: 50 }}>
+      <OrdersScreen />
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    paddingTop: 60,
-  },
-});
---
+++ uber-eats-courier/src/screens/OrdersScreen/index.js
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import orders from '../../../assets/data/orders.json';
+import OrderItem from '../../components/OrderItem';
+
+const OrdersScreen = () => {
+  const bottomSheetRef = useRef(null);
+
+  return (
+    <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
+      <BottomSheet ref={bottomSheetRef} snapPoints={['12%', '95%']}>
+        <View style={{ alignItems: 'center', marginBottom: 30 }}>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: '600',
+              letterSpacing: 0.5,
+              paddingBottom: 5,
+            }}
+          >
+            You're Online
+          </Text>
+          <Text
+            style={{
+              letterSpacing: 0.5,
+              color: 'gray',
+            }}
+          >
+            Available Orders: {orders.length}
+          </Text>
+        </View>
+        <FlatList
+          data={orders}
+          renderItem={({ item }) => <OrderItem order={item} />}
+        />
+      </BottomSheet>
+    </View>
+  );
+};
+
+export default OrdersScreen;
