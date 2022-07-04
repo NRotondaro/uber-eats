7f42449bf35ab668c0de3f1511dfb702a1eb0b2a
NRotondaro <niksonrotondaro1@gmail.com>
2022-07-04 15:23:49 +0000

    feat(order-delivery): finished the component with all the styles

--- uber-eats-courier/src/screens/OrderDelivery/index.js
+++ uber-eats-courier/src/screens/OrderDelivery/index.js
@@ -1,6 +1,11 @@
 import { useMemo, useRef } from 'react';
 import { View, Text } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { FontAwesome5, Fontisto } from '@expo/vector-icons';
+import orders from '../../../assets/data/orders.json';
+import styles from './styles';
+
+const order = orders[0];
 
 const OrderDelivery = () => {
   const bottomSheetRef = useRef(null);
@@ -8,13 +13,44 @@ const OrderDelivery = () => {
   const snapPoints = useMemo(() => ['12%', '95%'], []);
 
   return (
-    <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
+    <View style={styles.container}>
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={snapPoints}
-        handleIndicatorStyle={{ backgroundColor: 'gray', width: 100 }}
+        handleIndicatorStyle={styles.handleIndicator}
       >
-        <Text>OrderDelivery</Text>
+        <View style={styles.handleIndicatorContainer}>
+          <Text style={styles.routeDetailsText}>14 min</Text>
+          <FontAwesome5
+            name='shopping-bag'
+            size={30}
+            color='#3FC060'
+            style={{ marginHorizontal: 10 }}
+          />
+          <Text style={styles.routeDetailsText}>5 km</Text>
+        </View>
+        <View style={styles.deliveryDetailsContainer}>
+          <Text style={styles.restaurantName}>{order.Restaurant.name}</Text>
+          <View style={styles.addressContainer}>
+            <Fontisto name='shopping-store' size={22} color='gray' />
+            <Text style={styles.addressText}>{order.Restaurant.address}</Text>
+          </View>
+
+          <View style={styles.addressContainer}>
+            <FontAwesome5 name='map-marker-alt' size={30} color='gray' />
+            <Text style={styles.addressText}>{order.User.address}</Text>
+          </View>
+
+          <View style={styles.orderDetailsContainer}>
+            <Text style={styles.orderItemText}>Onion Rings x1</Text>
+            <Text style={styles.orderItemText}>Big Mac x3</Text>
+            <Text style={styles.orderItemText}>Big Tasty x2</Text>
+            <Text style={styles.orderItemText}>Coca-Cola x1</Text>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Accept Order</Text>
+        </View>
       </BottomSheet>
     </View>
   );
